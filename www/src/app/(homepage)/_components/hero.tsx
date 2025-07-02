a473ad29188ec42ea811d86066dc6200ce1842ec
logcho <lsjcwma@gmail.com>
2025-07-02 13:05:07 -0500

    fix: attempt to fix gradient cut off on mobile view

--- www/src/app/(homepage)/_components/hero.tsx
+++ www/src/app/(homepage)/_components/hero.tsx
@@ -2,15 +2,16 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
     return (
-        <section className="flex flex-col items-center justify-center bg-[url('/images/bg.jpg')] bg-cover bg-center bg-no-repeat h-screen w-full" id="inicio">
+        <section
+            className="relative flex flex-col items-center justify-center bg-[url('/images/bg.jpg')] bg-cover bg-center bg-no-repeat h-screen w-full"
+            id="inicio"
+        >
             {/* Gradient Overlay */}
-            <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/50 to-transparent" />
+            <div className="absolute inset-0 z-0 bg-gradient-to-t from-black/70 via-black/50 to-transparent" />
+
             {/* Text Content */}
-            <div className="relative p-6 max-w-3xl text-center font-thin">
-                <h1
-                    className="text-4xl mb-4"
-                    style={{ color: "#ffce1b" }}
-                >
+            <div className="relative z-10 p-6 max-w-3xl text-center font-thin">
+                <h1 className="text-4xl mb-4" style={{ color: "#ffce1b" }}>
                     Optimiza Tu Crédito y Presenta Tus Impuestos Fácil y Seguro
                 </h1>
                 <h2 className="text-white text-lg">
@@ -23,7 +24,10 @@ export default function Hero() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <Button className="text-black text-md font-extrabold w-80 hover:brightness-110" style={{ background: "#ffce1b" }}>
+                        <Button
+                            className="text-black text-md font-extrabold w-80 hover:brightness-110"
+                            style={{ background: "#ffce1b" }}
+                        >
                             Empieza con una Consulta GRATIS
                         </Button>
                     </a>
@@ -33,14 +37,15 @@ export default function Hero() {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <Button className="text-white text-md font-extrabold w-80 hover:brightness-110" style={{ color: "#ffce1b" }}>
+                        <Button
+                            className="text-white text-md font-extrabold w-80 hover:brightness-110"
+                            style={{ color: "#ffce1b" }}
+                        >
                             Descarga aquí tu reporte GRATIS
                         </Button>
                     </a>
                 </div>
-
             </div>
-
         </section>
-    )
+    );
 }
